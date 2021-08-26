bdf58b1f0a6c964643c587f68f3490ad7be04122
LOLROW <32370118+LOLROW@users.noreply.github.com>
2021-08-26 15:21:06 -0500

    Add files via upload

---
+++ C# School Projects/MCSPC/MCSPC/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace MCSPC
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            int trials = Convert.ToInt32(Console.ReadLine());
+
+            int wins1 = 0;
+            int wins2 = 0;
+
+            int[] dice2 = new int[12];
+            
+            for (int i = 0; i < trials; i++)
+            {
+                for (int z = 0; z < 6; z++)
+                {
+                    if (rand.Next(7) == 1)
+                    {
+                        wins1++;
+                        break;
+                    }
+                }
+
+                for (int j = 0; j < 12; j++)
+                {
+                    dice2[j] = rand.Next(7);
+                    
+                }
+
+                int amount = 0;
+                foreach (int x in dice2)
+                {
+                    if (x == 1)
+                    {
+                        amount++;
+                        if (amount >= 2)
+                        {
+                            wins2++;
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine((double) wins1 / trials);
+            Console.WriteLine((double) wins2 / trials);
+        }
+    }
+}
---
+++ C# School Projects/MonteCarloSimulation/MonteCarloSimulation/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MonteCarloSimulation
+{
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			Random rand = new Random();
+			
+			int stake = Convert.ToInt32(Console.ReadLine());
+			int goal = Convert.ToInt32(Console.ReadLine());
+			int trials = Convert.ToInt32(Console.ReadLine());
+
+			int wins = 0;
+			int loss = 0;
+			
+			for (int i = 0; i < trials; i++)
+			{
+				int money = stake;
+				while (money < 0 || money > goal)
+				{
+					if (rand.Next(0, 1) == 1)
+					{
+						money++;
+					}
+					else
+					{
+						money--;
+					}
+				}
+				if (money >= goal)
+				{
+					wins++;
+				}
+				else
+				{
+					loss++;
+				}
+			}
+			Console.WriteLine(100 * wins / trials);
+		}
+	}
+}
