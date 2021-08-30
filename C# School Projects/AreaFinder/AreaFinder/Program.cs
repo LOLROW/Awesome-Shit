21424bad441e8947b4e5897c70316d9bfc1e23af
LOLROW <32370118+LOLROW@users.noreply.github.com>
2021-08-30 13:53:19 -0500

    Add files via upload

---
+++ C# School Projects/AreaFinder/AreaFinder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AreaFinder
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int cases = Convert.ToInt32(Console.ReadLine()); int count = 0;
+            for (int i = 0; i < cases; i++)
+            {
+                float y = rand.Next(10);
+                if (y < myFxn(3))
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine((double) count / cases * 100);
+        }
+
+        public static float myFxn(float x)
+        {
+            return x; // Math.Sin(x);
+        }
+    }
+}
